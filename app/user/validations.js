5d4b5a3d4b736fc64fb4a4a08cb6aede4ac82acb
Muhammad Talha <muhammadtalha3810@gmail.com>
2021-05-19 14:45:56 +0500

    Create & Get API

--- app/user/validations.js
+++
@@ -1,8 +0,0 @@
-exports.validate = function (req, res, next) {
-
-    if (req.body.mobile_number) {
-        next();
-    } else {
-        res.status(400).send('Mobile number is required...');
-    }
-};
